b7fb829182bd98b62d9b7b6c754857e8c5836b40
azuer88 <blue.cuenca@gmail.com>
2015-04-18 16:15:12 +0800

    add testing for default values for Student object

--- registration/test_model.py
+++ registration/test_model.py
@@ -5,8 +5,10 @@ test the create of objects and check the different functions.
 make sure to do a proper test of each method and verify that the outputs are
 correct.
 """
+import datetime
+
 from django.test import TestCase
-from registration.models import Student, Address, Phone
+from registration.models import Student, Address, Phone, LEVEL_TYPES
 
 
 class StudentTestCase(TestCase):
@@ -43,3 +45,15 @@ class StudentTestCase(TestCase):
         self.assertEqual(fullname, "dela Cruz, Juan F.")
         fullname = Student.objects.get(last_name='Santiago').fullname
         self.assertEqual(fullname, "Santiago, Pablo")
+
+    def test_student_defaults(self):
+        obj = Student.objects.first()
+
+        # test some defaults
+        self.assertEqual(obj.birthdate, datetime.date.today())
+
+        self.assertEqual(obj.birthplace, '')
+
+        self.assertEqual(obj.get_school_level_display(), LEVEL_TYPES[1][1])
+
+        self.assertEqual(obj.year_level, 1)
