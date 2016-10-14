f2d42fa17cfb9d8ac45013a0146f5fb4f17cd335
Blue Cuenca <blue.cuenca@ymail.com>
2016-10-14 08:50:05 +0800

    test hotloading
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/assets/js/app.jsx
+++ src/assets/js/app.jsx
@@ -2,6 +2,6 @@ var React = require('react');
 
 module.exports = React.createClass({
     render: function() {
-        return <div><h1>Hello, World!</h1><h2>nice one.</h2><br/>errr...<br/>now....</div>
+        return <div><h1>Hello, World!</h1><h2>nice one.</h2><br/>errr...<br/>now....<br/>okay ni.</div>
     }
 });
