4ae8dea91bcc077eace56e3a644bfd0179801c8e
Blue Cuenca <blue.cuenca@ymail.com>
2016-11-07 08:18:16 +0800

    add docs app
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/cerberus/cerberus/urls.py
+++ src/cerberus/cerberus/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
 
     url(r'^$', TemplateView.as_view(template_name='index.html')),
 
+    url(r'^docs/', include('docs.urls', namespace='docs')),
+
     url(r'^admin/', include(admin.site.urls)),
 ]
